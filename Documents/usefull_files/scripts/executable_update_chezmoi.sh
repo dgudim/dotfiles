1e63a03ec398f18983d0b1458e24ce897f5c4260
kloud <dgudim@gmail.com>
2023-04-30 10:34:01 +0300

    Updated dotfiles

--- Documents/usefull_files/scripts/executable_update_chezmoi.sh
+++ Documents/usefull_files/scripts/executable_update_chezmoi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sleep 15
-
-notify-send -u critical "Dot files status" "$(chezmoi update)"
+chezmoi git fetch
+notify-send -u critical "Dot files status" "$(chezmoi git status)"
 paplay /usr/share/sounds/subnautica_theme/general_info.ogg
