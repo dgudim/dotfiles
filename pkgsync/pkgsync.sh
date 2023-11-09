9e4fdeea2865b699c2ca2dd869b82c3e6c7c580b
dgudim <dgudim@gmail.com>
2023-11-09 11:09:20 +0200

    Added some logic to install script + misc

--- pkgsync/pkgsync.sh
+++ pkgsync/pkgsync.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+L_YELLOW='\033[1;33m'
+L_BLUE='\033[1;34m'
 
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+YES_NO_MENU="(${GREEN}yes$NC/${YELLOW}no$NC/${BLUE}list$NC/${RED}abort${NC})..."
 
 # | ------------------------------------------------------------------------------- |
 # |                                                                                 |
@@ -48,14 +58,18 @@ cat "$TMP_DIR/local_install.list" "$TMP_DIR/global_install.list" | sort -u > "$T
 cat "$TMP_DIR/local_black.list" "$TMP_DIR/global_black.list"     | sort -u > "$TMP_DIR/combined_black.list"
 
 # 23 = remove lines unique to FILE2 and lines that appear in both files
-# To add = locally installed (explicit) - global install list - local install list (hardware)
-comm -23 "$TMP_DIR/explicitly_installed.list" "$TMP_DIR/combined_install.list" > "$TMP_DIR/pkg_to_add.list"
+# To add = locally installed (explicit) - global install list - local install list (hardware) - (blacklist (global) - blacklist (local))
+comm -23 "$TMP_DIR/explicitly_installed.list" "$TMP_DIR/combined_install.list" | comm -23 - "$TMP_DIR/combined_black.list" > "$TMP_DIR/pkg_to_add.list"
 
-# To remove = (blacklist (global) + blacklist (local)) (<<<-intersection with->>>) locally installed (explicit)
+# To remove = (blacklist (global) + blacklist (local)) <<<-intersection with->>> locally installed (explicit)
 comm -12 "$TMP_DIR/explicitly_installed.list" "$TMP_DIR/combined_black.list" > "$TMP_DIR/pkg_to_remove.list"
 
-# To install = global install list + local install list (hardware) - locally installed - blacklist (global) - blacklist (local)
+# To install = (global install list + local install list (hardware)) - (blacklist (global) - blacklist (local)) - locally installed (explicit)
 comm -23 "$TMP_DIR/combined_install.list" "$TMP_DIR/combined_black.list" | comm -23 - "$TMP_DIR/explicitly_installed.list" > "$TMP_DIR/pkg_to_install.list"
+
+# To remove from install list = (global install list / local install list (hardware)) <<<-intersection with->>> (blacklist (global) / blacklist (local))
+comm -12 "$TMP_DIR/global_install.list" "$TMP_DIR/global_black.list" | tr -d '[:space:]' > "$TMP_DIR/pkg_to_check.list"
+comm -12 "$TMP_DIR/local_install.list" "$TMP_DIR/local_black.list" | tr -d '[:space:]' >> "$TMP_DIR/pkg_to_check.list"
 
 # offer to install missing packages
 if [ -s "$TMP_DIR/pkg_to_install.list" ]
@@ -63,7 +77,7 @@ then
     yn=l
     while [[ ! "$yn" =~ ^[YyNnAa]$ ]]
     do
-        read -p "Install new packages? (yes/no/list/abort)..." -n 1 yn
+        read -p "$(echo -e ${GREEN}"Install"${NC} "new packages?" $YES_NO_MENU)" -n 1 yn
         echo
         [[ "$yn" =~ ^[Ll]$ ]] && cat "$TMP_DIR/pkg_to_install.list"
     done
@@ -82,7 +96,7 @@ then
     yn=l
     while [[ ! "$yn" =~ ^[YyNnAa]$ ]]
     do
-        read -p "Remove packages? (yes/no/list/abort)..." -n 1 yn
+        read -p "$(echo -e ${RED}"Remove"${NC} "packages?" $YES_NO_MENU)" -n 1 yn
         echo
         [[ "$yn" =~ ^[Ll]$ ]] && cat "$TMP_DIR/pkg_to_remove.list"
     done
@@ -97,11 +111,19 @@ then
     yn=l
     while [[ ! "$yn" =~ ^[YyNnAa]$ ]]
     do
-        read -p "Append packages unique to this computer to install list (yes/no/list/abort)..." -n 1 yn
+        read -p "$(echo -e ${PURPLE}"Append"$NC "packages unique to this computer to install list?" $YES_NO_MENU)" -n 1 yn
         echo
         [[ "$yn" =~ ^[Ll]$ ]] && cat "$TMP_DIR/pkg_to_add.list"
     done
     [[ "$yn" =~ ^[Yy]$ ]] && cat "$TMP_DIR/pkg_to_add.list" >> "$INSTALL_LIST"
     [[ "$yn" =~ ^[Aa]$ ]] && exit 0
+fi
+
+if [ -s "$TMP_DIR/pkg_to_check.list" ]
+then
+    echo -e "${L_YELLOW}There are packages both in install and remove list, maybe remove them?$NC"
+    echo -e "Packages:\n${L_BLUE}---------------------------------------$NC"
+    cat $TMP_DIR/pkg_to_check.list
+    echo -e "\n${L_BLUE}---------------------------------------$NC"
 fi
 
