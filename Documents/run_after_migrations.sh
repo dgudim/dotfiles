d1957774e2134b5e0c434d71f89914e096aaafc9
dgudim <dgudim@gmail.com>
2023-12-10 19:20:31 +0200

    Added migration for journalctl notifier

--- Documents/run_after_migrations.sh
+++ Documents/run_after_migrations.sh
@@ -12,4 +12,9 @@ rm -fv ~/.config/environment.d/xdg.conf
 rm -fv ~/.bash_history
 sudo rm -fv /root/.bash_history
 rm -fv ~/.config/dragonplayerrc
+
+# Make sure kde notification service is started instead of dunst
+mkdir -p ~/.local/share/dbus-1/services/
+ln -sf /usr/share/dbus-1/services/org.kde.plasma.Notifications.service ~/.local/share/dbus-1/services/org.kde.plasma.Notifications.service
+
 echo -e "${L_GREEN}Done!$NC\n"
