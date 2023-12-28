9371674485d16c6831ab3df277d26c01d363836f
dgudim <dgudim@gmail.com>
2023-12-28 16:46:38 +0200

    Fixed chezmoi update from the notification

--- Documents/usefull_files/scripts/executable_update_chezmoi.sh
+++ Documents/usefull_files/scripts/executable_update_chezmoi.sh
@@ -5,7 +5,7 @@ chezmoi git fetch
 paplay /usr/share/sounds/subnautica_theme/general_info.ogg &
 res=$(notify-send -u critical --action=yes="Update now" "Dot files status" "$(chezmoi git status)")
 if [ "$res" = "yes" ]; then
-    konsole -e '$SHELL -c "chezmoi update; $SHELL"' &
+    konsole -e 'bash --rcfile ~/.bash_aliases -ic "chzu; bash"' &
 fi
 
 check_conflicts() {
