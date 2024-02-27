1585276fb4ed286dda07319f77dafe0bb80133f0
dgudim <dgudim@gmail.com>
2024-02-27 08:57:05 +0200

    Some improvements to checker

--- Documents/run_after_checks.py
+++ Documents/run_after_checks.py
@@ -21,6 +21,7 @@ LIGHTER_GRAY = "\033[38;5;246m"
 
 print(f"{L_PURPLE}Running checks{NC}...")
 
+checks = 0
 warn = 0
 
 
@@ -33,43 +34,50 @@ def read_file(path: str) -> str:
     except Exception:
         return ""
 
+def run_check(cond: bool, message: str):
+    global warn
+    global checks
+
+    checks += 1
+    if cond:
+        print(message)
+        warn += 1
 
 def check_mount_opts(fstabb: str, fsopts: dict[str, list[str]]):
-    warn = 0
 
     for line in fstabb.split("\n"):
         if len(line) == 0 or line[0] == "#":
             continue
-        if line.find(" ntfs ") != -1:
-            # Replace with the new ntfs driver
-            print(f"{YELLOW}Consider replacing ntfs with ntfs3 {NC} ({line})")
-            warn += 1
+
+        # Replace with the new ntfs driver
+        run_check(line.find(" ntfs ") != -1,
+                  f"{YELLOW}Consider replacing ntfs with ntfs3 {NC} ({line})")
 
         dump_pass = re.search(r" (\d) *? (\d)", line)
         if len(dump_pass.groups()) == 2:
             fsck_pass = int(dump_pass.group(2).strip())
-            if fsck_pass > 2:
-                print(f"{YELLOW}Consider changing fsck field to 2 from {fsck_pass} {NC} ({line})")
-                warn += 1
 
-            if fsck_pass == 1 and line.find(" / ") == -1:
-                print(f"{YELLOW}Consider changing fsck field to 2 from {fsck_pass} {NC} (Only root should have pass value of 1) ({line})")
-                warn += 1
+            run_check(fsck_pass > 2,
+                      f"{YELLOW}Consider changing fsck field to 2 from {fsck_pass} {NC} ({line})")
 
-            if fsck_pass != 1 and line.find(" / ") != -1:
-                print(f"{YELLOW}Consider changing fsck field to 1 from {fsck_pass} {NC} (Root should have pass value of 1) ({line})")
-                warn += 1
+            run_check(fsck_pass == 1 and line.find(" / ") == -1,
+                      f"{YELLOW}Consider changing fsck field to 2 from {fsck_pass} {NC} (Only root should have pass value of 1) ({line})")
+
+
+            run_check(fsck_pass != 1 and line.find(" / ") != -1,
+                      f"{YELLOW}Consider changing fsck field to 1 from {fsck_pass} {NC} (Root should have pass value of 1) ({line})")
 
             btrfs_or_swap = line.find(" btrfs ") != -1 or line.find(" swap ") != -1
 
-            if fsck_pass == 0 and not btrfs_or_swap:
-                print(f"{YELLOW}Consider changing fsck field to 2 from {fsck_pass} {NC} ({line})")
-                warn += 1
+            run_check(fsck_pass == 0 and not btrfs_or_swap,
+                      f"{YELLOW}Consider changing fsck field to 2 from {fsck_pass} {NC} ({line})")
 
-            if fsck_pass != 0 and btrfs_or_swap:
-                print(f"{YELLOW}Consider changing fsck field to 0 from {fsck_pass} (Btrfs/swap does not need it) {NC} ({line})")
-                warn += 1
+            run_check(fsck_pass != 0 and btrfs_or_swap,
+                      f"{YELLOW}Consider changing fsck field to 0 from {fsck_pass} (Btrfs/swap does not need it) {NC} ({line})")
 
+        else:
+            run_check(line.find(" bind ") == -1,
+                      f"{YELLOW}Consider adding fsck field {NC} ({line})")
 
         for fs, opts in fsopts.items():
             if line.find(f" {fs} ") == -1:
@@ -77,22 +85,17 @@ def check_mount_opts(fstabb: str, fsopts: dict[str, list[str]]):
                 continue
             # Yay, we found our target filesystem
             for opt in opts:
-                if sum(1 for _ in re.finditer(f"({opt})", line)) != 1:
-                    print(f"{YELLOW}Consider adding {opt} to {fs}{NC} ({line})")
-                    warn += 1
+                run_check(sum(1 for _ in re.finditer(f"({opt})", line)) != 1,
+                          f"{YELLOW}Consider adding {opt} to {fs}{NC} ({line})")
             # A line can only contain 1 filesystem, exit, process next line
             break
-
-    return warn
 
 
 print(f"{LIGHTER_GRAY}Checking fstab{NC}")
 fstab = read_file("/etc/fstab")
 
 print(f"{LIGHT_GRAY}Checking tmp dir{NC}")
-if fstab.find("/tmp") != -1:
-    warn += 1
-    print(f"{YELLOW}Consider removing /tmp from fstab{NC}")
+run_check(fstab.find("/tmp") != -1, f"{YELLOW}Consider removing /tmp from fstab{NC}")
 
 print(f"{LIGHT_GRAY}Checking mount options{NC}")
 ntfs_mount_opts = [
@@ -107,7 +110,7 @@ ntfs_mount_opts = [
     "exec",
     "noatime",
 ]
-warn += check_mount_opts(
+check_mount_opts(
     fstab,
     {
         "ext4": ["defaults", "commit=60", "noatime"],
@@ -126,13 +129,11 @@ grub = read_file("/etc/default/grub")
 
 bootfile = cmdline if len(cmdline) > 0 else grub
 
-if len(bootfile) > 0 and bootfile.find("mitigations=off") == -1:
-    warn += 1
-    print(f"{YELLOW}Turn off mitigations in kernel cmdline{NC}")
+run_check(len(bootfile) > 0 and bootfile.find("mitigations=off") == -1,
+          f"{YELLOW}Turn off mitigations in kernel cmdline{NC}")
 
-if len(bootfile) > 0 and bootfile.find("rd.luks.options") != -1:
-    warn += 1
-    print(f"{YELLOW}Remove rd.luks.options from kernel cmdline{NC}")
+run_check(len(bootfile) > 0 and bootfile.find("rd.luks.options") != -1,
+          f"{YELLOW}Remove rd.luks.options from kernel cmdline{NC}")
 
 # https://wiki.archlinux.org/title/Dm-crypt/Specialties#Discard/TRIM_support_for_solid_state_drives_(SSD)
 if cmdline.find("rd.luks") != -1:
@@ -152,24 +153,16 @@ if cmdline.find("rd.luks") != -1:
         luks_root_flags = read_file("/tmp/luks_root_flags")
         os.system("rm /tmp/luks_root_flags")
 
-        if luks_root_flags.find("allow-discards") == -1 or luks_root_flags.find("no-read-workqueue") == -1 or luks_root_flags.find("no-write-workqueue") == -1:
-            print(
-                f"{YELLOW}sudo cryptsetup --perf-no_read_workqueue --perf-no_write_workqueue --allow-discards --persistent refresh {disk_dev_id} luks-{luks_root_uuid}{NC}"
-            )
-            warn += 1
+        run_check(luks_root_flags.find("allow-discards") == -1 or luks_root_flags.find("no-read-workqueue") == -1 or luks_root_flags.find("no-write-workqueue") == -1,
+                  f"{YELLOW}sudo cryptsetup --perf-no_read_workqueue --perf-no_write_workqueue --allow-discards --persistent refresh {disk_dev_id} luks-{luks_root_uuid}{NC}")
 
         luks_crypttab_opts = re.search(
             rf"UUID={luks_root_uuid} .*?\/crypto_keyfile\.bin .*?(.*)", crypttab
         )
 
-        if (
-            luks_crypttab_opts is not None
-            and luks_crypttab_opts.group(1).find("discard") != -1
-        ):
-            print(
-                f"{YELLOW}Consider removing discard from luks {L_BLUE}{luks_root_uuid}{YELLOW} in crypttab{NC}"
-            )
-            warn += 1
+        run_check(luks_crypttab_opts is not None and luks_crypttab_opts.group(1).find("discard") != -1,
+                  f"{YELLOW}Consider removing discard from luks {L_BLUE}{luks_root_uuid}{YELLOW} in crypttab{NC}")
+
     else:
         print(f"{RED}Couldn't find luks root in fstab!{NC}")
 
@@ -205,15 +198,13 @@ if len(firefox_profiles) == 0:
 else:
 
     def check_setting(s: str, setting: str) -> int:
-        if s.find(setting) == -1:
-            print(f"{YELLOW}Consider adding {setting} to firefox{NC}")
-            return 1
-        return 0
+        run_check(s.find(setting) == -1, f"{YELLOW}Consider adding {setting} to firefox{NC}")
 
     print(f"{GRAY}Selected firefox profile: {firefox_profiles[0]}{NC}")
 
     about_config = read_file(os.path.join(firefox_profiles[0], "prefs.js"))
-    for setting in firefox_settings:
-        warn += check_setting(about_config, setting)
 
-print(f"{L_GREEN}Finished running checks, {L_CYAN}{warn}{L_GREEN} warning(s){NC}\n")
+    for setting in firefox_settings:
+        check_setting(about_config, setting)
+
+print(f"{L_GREEN}Finished running checks, {L_CYAN}{warn} / {checks}{L_GREEN} warning(s){NC}\n")
