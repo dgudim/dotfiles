5cf9a82c54dac213775ec48b8b539e6620c15daf
dgudim <dgudim@gmail.com>
2025-07-09 23:46:42 +0300

    bump version of udev-notify-my

--- pkgsync/pkg_build.sh
+++ pkgsync/pkg_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! yay -Q | grep udev-no | cut -d ' ' -f2 | grep -q 0.3.3; then
+if ! yay -Q | grep udev-no | cut -d ' ' -f2 | grep -q 0.3.4; then
     target_dir="/tmp/yay/udev-notify-my"
     mkdir -pv "$target_dir"
     cp -vf pkgbuilds/udev-notify-PKGBUILD "$target_dir/PKGBUILD"
