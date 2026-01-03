a2ce16bc1b17054341ab165e0a026ed4d8bb8e34
dgudim <dgudim@gmail.com>
2026-01-03 20:19:22 +0200

    Update termux scripts

--- Documents/termux/.shortcuts/download_artworks.sh
+++ Documents/termux/.shortcuts/download_artworks.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
-PENDING_ARTSTATION_IMAGES_FILE_LOCATION="~/storage/downloads/artworks/pending.txt"
-PIXIV_IMAGES_SOURCE_DIR="~/storage/shared/Pictures/PixivFunc/"
-BASE_PERSONAL_DIR="/storage/3538-3530/Documents/personal"
+export PENDING_ARTSTATION_IMAGES_FILE_LOCATION="$HOME/storage/downloads/artworks/pending.txt"
+export PIXIV_IMAGES_SOURCE_DIR="$HOME/storage/shared/Pictures/PixivFunc/"
+export BASE_PERSONAL_DIR="/storage/3538-3530/Documents/personal"
 
 mkdir -pv ~/.temp_artwork_downloader
 cp -fv ~/dotfiles/Documents/exact_usefull_files/exact_scripts/artwork_downloader/download_and_sort.py ~/.temp_artwork_downloader/download_and_sort.py
